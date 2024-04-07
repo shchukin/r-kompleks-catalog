514c2f6e055f90a82bb43e420b3ef6f00d4c2ffb
Anton Shchukin <a.a.shchukin@gmail.com>
2024-04-07 22:40:45 +0700

    slider clean up

--- src/scripts/_project.js
+++ src/scripts/_project.js
@@ -1,19 +1,26 @@
 (function ($) {
 
-    var swiper = new Swiper(".mySwiper", {
+    var swiper = new Swiper('.project__navigation .swiper', {
         spaceBetween: 17,
         slidesPerView: 5,
+        slidesPerGroup: 5,
         freeMode: true,
         watchSlidesProgress: true,
+        mousewheel: {
+            forceToAxis: true,
+        },
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
     });
 
-    var swiper2 = new Swiper(".mySwiper2", {
+    var swiper2 = new Swiper('.project__stage .swiper', {
         autoHeight: true,
         spaceBetween: 10,
+        mousewheel: {
+            forceToAxis: true,
+        },
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
