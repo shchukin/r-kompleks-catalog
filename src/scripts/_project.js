429d838f890d32b0511cf767ef874af8163e0844
Anton Shchukin <a.a.shchukin@gmail.com>
2024-04-07 22:01:37 +0700

    previews

--- src/scripts/_project.js
+++ src/scripts/_project.js
@@ -1,12 +1,18 @@
 (function ($) {
 
     var swiper = new Swiper(".mySwiper", {
-        spaceBetween: 10,
-        slidesPerView: 4,
+        spaceBetween: 17,
+        slidesPerView: 5,
         freeMode: true,
         watchSlidesProgress: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
     });
+
     var swiper2 = new Swiper(".mySwiper2", {
+        autoHeight: true,
         spaceBetween: 10,
         navigation: {
             nextEl: ".swiper-button-next",
@@ -16,5 +22,7 @@
             swiper: swiper,
         },
     });
+
+
 
 })(jQuery);
