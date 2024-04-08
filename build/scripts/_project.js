3d68ba488418938b945750d127e3b178c55b71ca
Anton Shchukin <a.a.shchukin@gmail.com>
2024-04-08 13:40:17 +0700

    build

---
+++ build/scripts/_project.js
@@ -0,0 +1,55 @@
+(function ($) {
+
+
+    /* Навигация (инициализируется первой) */
+
+    const swiperNavigation = new Swiper('.project__navigation .swiper', {
+        spaceBetween: 17,
+        slidesPerView: 5,
+        slidesPerGroup: 5,
+        mousewheel: {
+            forceToAxis: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        /* Из документации: */
+        freeMode: true,
+        watchSlidesProgress: true,
+    });
+
+
+    /* Сцена (инициализируется после) */
+
+    const swiperStage = new Swiper('.project__stage .swiper', {
+        spaceBetween: 10,
+        mousewheel: {
+            forceToAxis: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        thumbs: {
+            swiper: swiperNavigation, // связь с навигацией
+        },
+    });
+
+
+    $('.project__stage').magnificPopup({
+        delegate: '.preview--stage',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1],
+            tCounter: '<span class="mfp-counter-index">%curr%/%total%</span>'
+        }
+
+    });
+
+
+})(jQuery);
